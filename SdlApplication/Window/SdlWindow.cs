5ceff7b6151e8d939c1132a45606c8096cbbdc6d
Dmitry Filipuk <kupilifds@gmail.com>
2018-09-19 20:59:24 +0300

    additional task: painting

--- SdlApplication/Window/SdlWindow.cs
+++ SdlApplication/Window/SdlWindow.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using SdlApplication.Limacon;
 using SDL2;
@@ -22,7 +24,7 @@ namespace SdlApplication.Window
             _title = title;
             _screenHeight = screenHeight;
             _screenWidth = screenWidth;
-            _limaconDrawer = new Drawer(LimaconType.TypeA);
+            _limaconDrawer = new Drawer(LimaconType.TypeC);
         }
 
         public void Open()
@@ -90,9 +92,42 @@ namespace SdlApplication.Window
             SDL.SDL_GetWindowSize(_window, out width, out height);
 
             SDL.SDL_Point[] points = _limaconDrawer.Draw(width, height);
+
+            PaintLimacon(points, height / 2, 255, 255, 0);
+
+            SDL.SDL_SetRenderDrawColor(_renderer, 255, 255, 255, 255);
             SDL.SDL_RenderDrawPoints(_renderer, points, points.Length);
 
             SDL.SDL_RenderPresent(_renderer);
+        }
+
+        private void PaintLimacon(SDL.SDL_Point[] points, int centerY, byte r, byte g, byte b)
+        {
+            var zeroPoints = points.Where(p => Math.Abs(p.y - centerY) == 0).ToArray();
+            zeroPoints = zeroPoints.OrderBy(p => p.x).ToArray();
+            var minX = zeroPoints[0];
+            var maxX = zeroPoints[zeroPoints.Length - 2];
+
+            var newPoints = points.Where(p => (p.x >= minX.x && p.x <= maxX.x));
+
+            SDL.SDL_SetRenderDrawColor(_renderer, r, g, b, 255);
+
+            foreach (var point in newPoints)
+            {
+                
+                var curPoints = points.Where(p => p.x == point.x).OrderBy(p => p.y).ToArray();
+                var temp = new List<SDL.SDL_Point>();
+
+                foreach (var p in curPoints)
+                {
+                    temp.Add(new SDL.SDL_Point() { x = p.x, y = p.y - centerY });
+                }
+
+                var minusPoints = temp.Where(p => p.y <= 0).OrderBy(p => p.y).ToArray();
+                var positivPoints = temp.Where(p => p.y > 0).OrderBy(p => p.y).ToArray();
+
+                SDL.SDL_RenderDrawLine(_renderer, minusPoints[minusPoints.Length - 1].x, minusPoints[minusPoints.Length - 1].y + centerY, positivPoints[0].x, positivPoints[0].y + centerY);
+            }
         }
     }
 }
